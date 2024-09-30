a1ce828fdafaedcd8063a98098fc0101419758d0
Egor Korplyakov <korplyakov00@mail.ru>
2024-09-30 09:48:11 +0300

    UPDATE: Some fixes and changes

---
+++ js/reviews-swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper("#reviews-swiper", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: true,
+  },
+  spaceBetween: 20,
+});
+
