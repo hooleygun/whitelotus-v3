fcd0fe2f08f0547e952d9e559553f106b7a76dd2
Egor Korplyakov <korplyakov00@mail.ru>
2024-10-02 12:58:08 +0300

    UPD: Number input fix

--- js/order-module.js
+++ js/order-module.js
@@ -40,12 +40,40 @@ function orderModuleOpen() {
   body.style.overflowY = 'clip';
 }
 
-function checkCheckbox(){
-  let checkbox = document.getElementById("acception-radio-button");
+function validation(){
+  let roolsCheckbox = document.getElementById("order-rools-acception-checkbox");
+  let personalDataCheckbox = document.getElementById("order-personal-data-acception-radio-button");
   let orderButton = document.getElementById("order-button");
-  if (checkbox.checked) {
+
+  let nameInput = document.getElementById("order-name");
+  let numberInput = document.getElementById("order-number");
+  let checkInDateInput = document.getElementById("order-check-in-date");
+  let departureDateInput = document.getElementById("order-departure-date");
+  let guestsQuantityInput = document.getElementById("order-guests-quantity");
+  let roomsCategoryInput = document.getElementById("order-rooms-category");
+  let emailInput = document.getElementById("order-email");
+
+  if ((roolsCheckbox.checked) && (personalDataCheckbox.checked) && (nameInput.checkValidity() == true) && (numberInput.checkValidity() == true)
+    && (checkInDateInput.checkValidity() == true)  && (departureDateInput.checkValidity() == true)  && (guestsQuantityInput.checkValidity() == true)  && (roomsCategoryInput.checkValidity() == true)  && (emailInput.checkValidity() == true)) {
     orderButton.disabled = false;
   } else {
     orderButton.disabled = true;
   }
 }
+
+const date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+let checkInDateInput = document.getElementById("order-check-in-date");
+let departureDateInput = document.getElementById("order-departure-date");
+if (month < 10){
+  month = "0"+month;
+}
+if (day < 10){
+  day = "0"+day;
+}
+let currentDate = year+"-"+month+"-"+day;
+checkInDateInput.min = currentDate;
+departureDateInput.min = currentDate;
