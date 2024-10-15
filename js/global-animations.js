522c0033499e368ca96c34e9b0b0571cd2f754c5
Egor Korplyakov <korplyakov00@mail.ru>
2024-10-15 10:52:02 +0300

    IDK

--- js/global-animations.js
+++ js/global-animations.js
@@ -1,23 +1,23 @@
-const animItemsL1 = document.querySelectorAll('._anim-item-l1');
+const animCards = document.querySelectorAll('._anim-card');
 
-if (animItemsL1.length > 0){
+if (animCards.length > 0){
   window.addEventListener('scroll', animOnScroll);
   function animOnScroll(params){
-    for (i = 0; i < animItemsL1.length; i++){
-      const animItemL1 = animItemsL1[i];
-      const animItemL1Height = animItemL1.offsetHeight;
-      const animItemL1Offset = offset(animItemL1).top;
+    for (i = 0; i < animCards.length; i++){
+      const animCard = animCards[i];
+      const animCardHeight = animCard.offsetHeight;
+      const animCardOffset = offset(animCard).top;
       const animStart = 3;
 
-      let animItemL1Point = window.innerHeight - animItemL1Height / animStart;
+      let animCardPoint = window.innerHeight - animCardHeight / animStart;
 
-      if (animItemL1Height > window.innerHeight){
-        animItemL1Point = window.innerHeight - window.innerHeight / animStart;
+      if (animCardHeight > window.innerHeight){
+        animCardPoint = window.innerHeight - window.innerHeight / animStart;
       }
 
-      if ((scrollY > animItemL1Offset - animItemL1Point) && scrollY < (animItemL1Offset + animItemL1Height)){
-        animItemL1.classList.add('_element-active-l1');
-      } //else { animItemL1.classList.remove('_element-active-l1'); }
+      if ((scrollY > animCardOffset - animCardPoint) && scrollY < (animCardOffset + animCardHeight)){
+        animCard.classList.add('_element-active-l1');
+      } //else { animCardL1.classList.remove('_element-active-l1'); }
     }
   };
   function offset(el){
